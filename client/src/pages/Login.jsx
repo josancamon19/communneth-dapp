7e7a5e012781da549d369bfc8d130b16bd42dd71
Joan <joan.santiago.cabezas@gmail.com>
2022-03-22 08:20:38 -0500

    Login js form validation

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -10,18 +10,10 @@ import {
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { Box } from "@mui/system";
+import { Formik, ErrorMessage } from "formik";
 
 function Login() {
   const navigate = useNavigate();
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
-  };
 
   useEffect(() => {
     const queryParams = new URLSearchParams(window.location.search);
@@ -44,42 +36,95 @@ function Login() {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            placeholder="communeth/1/conjuntoloscerezos/proto"
-            id="channel"
-            label="Channel"
-            name="channel"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="/new-channel" variant="body2">
-                Create channel
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
+        <Formik
+          initialValues={{ channel: "", password: "" }}
+          validate={(values) => {
+            const errors = {};
+            if (values.channel === "") {
+              errors.channel = "Channel field required";
+            }
+            if (values.password === "") {
+              errors.password = "Password field required";
+            }
+            if (errors.channel === undefined) {
+              if (values.channel.split("/").length !== 4) {
+                errors.channel = "Invalid channel value";
+              }
+            }
+
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            console.log(values);
+            // TODO go to contract and find channel matches password
+            // TODO if so, 
+            // - save in local storage using store.js
+            // - go to home
+            setSubmitting(false);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+          }) => (
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                placeholder="communeth/1/conjuntoloscerezos/proto"
+                id="channel"
+                label="Channel"
+                name="channel"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.channel}
+                fullWidth
+                autoFocus
+                required
+              />
+              {errors.channel && touched.channel && errors.channel}
+              <TextField
+                margin="normal"
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+                fullWidth
+                autoFocus
+                required
+              />
+              {errors.password && touched.password && errors.password}
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                disabled={isSubmitting}
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="/new-channel" variant="body2">
+                    Create channel
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          )}
+        </Formik>
       </Box>
     </Container>
   );
